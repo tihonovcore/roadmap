16795b4dbe7a4ad7b8a8bf045526294ade9cea6a
Tikhonov Vitaly <tihonovcore@gmail.com>
2025-07-20 13:32:49 +0300

    add ui and gradle files

---
+++ app/src/main/java/com/example/roadmap/ui/RoadmapApp.kt
@@ -0,0 +1,35 @@
+package com.example.roadmap.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.roadmap.ui.theme.RoadmapTheme
+
+@Composable
+fun RoadmapApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(Modifier.padding(innerPadding)) {
+            Text(
+                text = "hello, map text",
+                modifier = Modifier.padding(innerPadding),
+            )
+            Button(onClick = {}) {
+                Text(text = "hello, map button")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AppPreview() {
+    RoadmapTheme {
+        RoadmapApp()
+    }
+}
---
+++ app/src/main/java/com/example/roadmap/ui/RoadmapsListScreen.kt
@@ -0,0 +1,2 @@
+package com.example.roadmap.ui
+
