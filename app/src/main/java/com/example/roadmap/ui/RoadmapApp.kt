bec0c4c7c3bac278e00a8049bd02c08aa039ee6e
Tikhonov Vitaly <tihonovcore@gmail.com>
2025-07-20 20:27:04 +0300

    add top bar

--- app/src/main/java/com/example/roadmap/ui/RoadmapApp.kt
+++ app/src/main/java/com/example/roadmap/ui/RoadmapApp.kt
@@ -3,15 +3,45 @@ package com.example.roadmap.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.roadmap.R
 import com.example.roadmap.ui.theme.RoadmapTheme
 
 @Composable
 fun RoadmapApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            @OptIn(ExperimentalMaterial3Api::class)
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Roadmap")
+                },
+                navigationIcon = {
+                    if (false /*TODO*/) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = stringResource(R.string.back_button)
+                            )
+                        }
+                    }
+                }
+
+            )
+        }
+    ) { innerPadding ->
         Column(Modifier.padding(innerPadding)) {
             RoadmapsList()
         }
